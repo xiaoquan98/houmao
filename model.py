41ccd61d98920732f6ab23c5befbf110c37da356
xiaoquan98 <xiaoquan98@gmail.com>
2015-10-04 09:02:56 +0800

    indent with zed editor.

--- model.py
+++ model.py
@@ -3,39 +3,40 @@ import web
 import sys 
 default_encoding = 'utf-8' 
 if sys.getdefaultencoding() != default_encoding: 
-	reload(sys) 
-	sys.setdefaultencoding(default_encoding) 
-# 	print   "sys encode :%s" % sys.getdefaultencoding() 
+    reload(sys) 
+    sys.setdefaultencoding(default_encoding) 
+#     print   "sys encode :%s" % sys.getdefaultencoding() 
 # else:
-# 	print "sys encode utf-8"
+#     print "sys encode utf-8"
+
 
 db = web.database(dbn='mysql', db='myissuedb', user='root',pw='idore',charset='utf8')
 
 def get_issues():
-	return db.select('issue', order='id')
+    return db.select('issue', order='id')
 
 def  get_issue(id):
-	return db.select('issue',where="id=$id", vars=locals())
+    return db.select('issue',where="id=$id", vars=locals())
 
 def new_issue(title,detail,parent,user,isArticle):
- 	n = db.insert('issue',title=title,detail=detail,parent=parent,user=user,isArticle=isArticle)
- 	return n;
+     n = db.insert('issue',title=title,detail=detail,parent=parent,user=user,isArticle=isArticle)
+     return n;
 
 def del_issue(id):  
-	db.delete('issue', where="id=$id", vars=locals())
+    db.delete('issue', where="id=$id", vars=locals())
 
 def get_users():
-	return db.select('user', order='id')
+    return db.select('user', order='id')
 
 def  get_user(id):
-	return db.select('user',where="id=$id", vars=locals())
+    return db.select('user',where="id=$id", vars=locals())
 
 def new_user(name,password):
-	n = db.insert('user',name=name,password=password)
-	return n
+    n = db.insert('user',name=name,password=password)
+    return n
 
 if __name__ == '__main__':
-	n = new_issue("hi",u"and hello大饭店梵蒂冈203",1,1,True);
-	issues= get_issue(n)
-	for  issue in issues:
-        		print issue.title, issue.detail
+    n = new_issue("hi",u"and hello大饭店梵蒂冈203",1,1,True);
+    issues= get_issue(n)
+    for  issue in issues:
+        print issue.title, issue.detail
--- static/myapp.js
+++ static/myapp.js
@@ -1,25 +1,23 @@
-
-
-    var postApp = angular.module('issueApp', []);
-    postApp.controller('postController', ['$scope', '$http',  function($scope, $http) {
-       $scope.input = {};
-         $scope.submitForm = function() {
-        $http({
-          method  : 'POST',
-          url     : '/json/issue/0',// new issue always use this address.
-          data    : $scope.input, 
-          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-         })
-          .success(function(data) { // get return data here.
-            if (data.errors) {
-              $scope.errorName = data.errors.name;
-              $scope.errorUserName = data.errors.username;
-              $scope.errorEmail = data.errors.email;
-              // alert("error happens.");
-            } else {
-              $scope.message = data.message;
-              // alert(data.message[0].detail);
-            }
-          });
-        };
-    }]);
+var postApp = angular.module('issueApp', []);
+postApp.controller('postController', ['$scope', '$http',  function($scope, $http) {
+   $scope.input = {};
+   $scope.submitForm = function() {
+    $http({
+      method  : 'POST',
+      url     : '/json/issue/0',// new issue always use this address.
+      data    : $scope.input, 
+      headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+     })
+      .success(function(data) { // get return data here.
+        if (data.errors) {
+          $scope.errorName = data.errors.name;
+          $scope.errorUserName = data.errors.username;
+          $scope.errorEmail = data.errors.email;
+          // alert("error happens.");
+        } else {
+          $scope.message = data.message;
+          // alert(data.message[0].detail);
+        }
+      });
+    };
+}]);
